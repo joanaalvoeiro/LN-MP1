e17668cc3bc4aafe31e99b3cbd16dec417beec13
amargaridaferro <margaridaferro@tecnico.ulisboa.pt>
2020-10-18 16:07:49 +0100

    ->take out .lower in standardize(). Lower case is in stem for default

--- qc.py
+++ qc.py
@@ -4,6 +4,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.snowball import SnowballStemmer
 from nltk.metrics.distance import jaccard_distance
+from nltk import bigrams
 
 class Question:
     def __init__(self, coarse_lbl, fine_lbl, question):
@@ -13,6 +14,7 @@ class Question:
 
 test_global_questions = []
 chosen_global_questions = []
+
 
 def parse_training_file(filename):
     questions = []
@@ -27,10 +29,10 @@ def parse_training_file(filename):
         coarse_lbl = line[:fine_separator]
         fine_lbl = line[:question_separator]
         question = preprocess_question(line[question_separator:].strip())
-
         questions.append(Question(coarse_lbl, fine_lbl, question))
     
     return questions
+
 
 def parse_test_file(filename):
     questions = []
@@ -48,33 +50,42 @@ def parse_test_file(filename):
 def preprocess_question(question):
     return stem(remove_stopwords(tokenize(standardize(question))))
 
+
 def standardize(question):
     #make everything lowercase and remove punctuation and some symbols
-    return re.sub("[?|\.|!|:|,|;|`|'|\"]", '', question).lower()
+    return re.sub("[?|\.|!|:|,|;|`|'|\"]", '', question)
+
 
 def tokenize(question):
     return word_tokenize(question)
 
+
+def bigrams_aux(question):
+    return list(bigrams(question))
+
+
 def remove_stopwords(question):
     question_words = set(['what', 'which', 'who', 'why', 'when', 'how', 'where', 'whose'])
     extra_stopwords = ['&', 'first', 'second', 'go', 'one', 'two', 'four','five','%','=']
-    stopword_set = set(stopwords.words('english') + extra_stopwords) - question_words
+    stopword_set = set(stopwords.words('english')+ extra_stopwords) - question_words
 
     filtered_question = [w for w in question if not w in stopword_set]
-
     return filtered_question
+
 
 def stem(question):
     stemmer = SnowballStemmer("english")
     stemmed_question = [stemmer.stem(w) for w in question ]
     return stemmed_question
 
+
 def get_label(question, coarseness):
     if(coarseness == "-coarse"):
         return question.coarse_lbl
     elif(coarseness == "-fine"):
         return question.fine_lbl
-        
+
+
 def predict_labels(test_questions, known_questions, coarseness):
     labels = []
     n_known_questions = len(known_questions)
@@ -85,14 +96,13 @@ def predict_labels(test_questions, known_questions, coarseness):
         for i in range(n_known_questions):
             dist = jaccard_distance(set(test_question), set(known_questions[i].question))
             if(dist < smallest_dist):
-                smallest_dist = dist
-                closest_question = known_questions[i]
+                 smallest_dist = dist
+                 closest_question = known_questions[i]
         if(closest_question != None):
             labels.append(get_label(closest_question, coarseness))
             chosen_global_questions.append(closest_question.question)
         else:
-            labels.append(None)
-
+             labels.append(None)
     return labels
 
 
@@ -113,14 +123,14 @@ def main():
     with open("treated_known_questions.txt", 'w') as known:
         for known_question in chosen_global_questions:
             for item in known_question:
-                known.write(item + " ")
+                known.write(str(item) + " ")
             known.write("\n")
     known.close()
 
     with open("treated_test_questions.txt", 'w') as test:
         for test_question in test_global_questions:
             for item in test_question:
-                test.write(item + " ")
+                test.write(str(item) + " ")
             test.write("\n")
     test.close()
 
