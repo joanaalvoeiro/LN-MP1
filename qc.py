eeda192721b3d1904c11994a751bb0e7b63d7837
joanaalvoeiro <joanaalvoeiro@tecnico.ulisboa.pt>
2020-10-22 20:04:37 +0100

    tried tf idf, big sad

--- qc.py
+++ qc.py
@@ -18,6 +18,55 @@ class Question:
 test_global_questions = []
 chosen_global_questions = []
 
+#----------------------------------------------------------------------------------
+#           methods we tested but didnt end up using
+#----------------------------------------------------------------------------------
+
+def cosine_similarity(a1, a2):
+    return np.dot(a1, np.transpose(a2)) / (np.linalg.norm(a1) * np.linalg.norm(a2))
+
+def tf_idf(test_questions, known_questions):
+    questions_list = [test_questions, [q.question for q in known_questions]]
+    tfs = [[], []]
+    doc_terms = [{}, {}]
+    terms = set([])
+
+    for qn in range(len(questions_list)):
+        questions = questions_list[qn]
+        n_questions = len(questions)
+        for i in range(n_questions):
+            tfs[qn].append({})
+            for word in questions[i]:
+                terms.add(word)
+                if(word in tfs[qn][i]):
+                    tfs[qn][i][word] += 1/len(questions[i])
+                else:
+                    tfs[qn][i][word] = 1/len(questions[i])
+                    if(word in doc_terms[qn]):
+                        doc_terms[qn][word] += 1
+                    else:
+                        doc_terms[qn][word] = 1
+
+    terms = list(terms)
+    n_terms = len(terms)
+
+    test_tf_idf = np.zeros((len(questions_list[0]), n_terms))
+    known_tf_idf = np.zeros((len(questions_list[1]), n_terms))
+    tf_idf = [test_tf_idf, known_tf_idf]
+    
+    for qn in range(len(questions_list)):
+        n_questions = len(questions_list[qn])
+        for i in range(n_questions):
+            for j in range(len(terms)):
+                if(terms[j] in tfs[qn][i]):
+                    df = doc_terms[qn][terms[j]]
+                    tf_idf[qn][i][j] = tfs[qn][i][terms[j]] * np.log(n_questions/df + 1)
+
+    return tf_idf[0], tf_idf[1]
+
+#----------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------
+
 
 def parse_training_file(filename,coarseness):
     questions = []
@@ -149,48 +198,22 @@ def predict_labels_tf_idf(test_questions, known_questions, coarseness):
     labels = []
     n_known_questions = len(known_questions)
 
-    #for test_question in test_questions:
-    #   best_score
+    for i in range(len(test_questions)):
+        test_global_questions.append(test_questions[i])
+        best_similarity = 0
+        closest_question = None
+        for j in range(n_known_questions):
+            similarity = cosine_similarity(test_tf_idf[i], known_tf_idf[j])
+            if(similarity > best_similarity):
+                 best_similarity = similarity
+                 closest_question = known_questions[j]
+        if(closest_question != None):
+            labels.append(get_label(closest_question, coarseness))
+            chosen_global_questions.append(closest_question.question)
+        else:
+             labels.append(None)
 
     return labels
-
-
-def tf_idf(test_questions, known_questions):
-    questions_list = [test_questions, [q.question for q in known_questions]]
-    tfs = [[], []]
-    doc_terms = [{}, {}]
-    terms = set([])
-
-    for qn in range(len(questions_list)):
-        questions = questions_list[qn]
-        n_questions = len(questions)
-        for i in range(n_questions):
-            tfs[qn].append({})
-            for word in questions[i]:
-                terms.add(word)
-                if(word in tfs[qn][i]):
-                    tfs[qn][i][word] += 1/len(questions[i])
-                else:
-                    tfs[qn][i][word] = 1/len(questions[i])
-                    if(word in doc_terms[qn]):
-                        doc_terms[qn][word] += 1
-                    else:
-                        doc_terms[qn][word] = 1
-
-    terms = list(terms)
-    tf_idf = np.zeros((2, n_questions, len(terms), 1))
-    
-    for qn in range(len(questions_list)):
-        n_questions = len(questions_list[qn])
-        for i in range(n_questions):
-            for j in range(len(terms)):
-                if(terms[j] in tfs[qn][i]):
-                    df = doc_terms[qn][terms[j]]
-                    tf_idf[qn][i][j] = tfs[qn][i][terms[j]] * np.log(n_questions/df + 1)
-                else:
-                    tf_idf[qn][i][j] = 0
-
-    return tf_idf[0], tf_idf[1]
      
 
 def main():
@@ -201,8 +224,6 @@ def main():
     known_questions = parse_training_file(training_file,coarseness)
 
     test_questions = parse_test_file(test_file,coarseness)
-
-    #now time to compare and decide stuff
 
     labels = predict_labels(test_questions, known_questions, coarseness)
 
